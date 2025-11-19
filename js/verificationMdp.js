5bc2d20297bfd89835e2b961b6b05a0586c509c7
antonin martel <antonin.mrtl.pr@gmail.com>
2025-11-19 19:53:25 +0100

    Add regex username

--- js/verificationMdp.js
+++ js/verificationMdp.js
@@ -3,12 +3,14 @@ let type = document.getElementById("mdp");
 let maj = document.getElementById("Majuscule");
 let min = document.getElementById("Minuscule");
 let nbr = document.getElementById("Chiffre");
+let symb = document.getElementById("Symbols");
 let longueur = document.getElementById("caractere");
 let bouton = document.getElementById("monbouton");
 let confirme = document.getElementById("mdp2");
 let user = document.getElementById("username");
 let mail = document.getElementById("email");
 let myMessageerreur = document.getElementById("message");
+let myMessageerreur2 = document.getElementById("message2");
 let barrePourcentage = document.getElementById("pourcentage");
 
 let myinputUser = document.getElementById("username");
@@ -18,14 +20,17 @@ let patternmaj = "(?=.*[A-Z])";
 let patternmin = "(?=.*[a-z])";
 let patternnbr = "(?=.*[0-9])";
 let patternlongueur = "(?=.*.{8,})";
-let tout = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*.{8,})";
+let patternusername = "(?=.*.{3,})";
+let patternsymbole = "(?=.*[^a-zA-Z0-9])";
+let tout = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*.{8,})(?=.*[^a-zA-Z0-9])";
 let les2mdps = false;
 let mdpFort = false;
 let mailBon = false;
+let userBon = false;
 
 bouton.addEventListener("click", enrengistrerMdp);
 myinput.addEventListener("keyup", verifMdp);
-mdp2.addEventListener("keyup", confirmMdp);
+confirme.addEventListener("keyup", confirmMdp);
 
 eyes.addEventListener("click", function () {
   if (eyes.src.includes("on")) {
@@ -42,15 +47,23 @@ function verifMdp() {
   if (myinput.value.match(patternmaj)) {
     maj.classList.remove("nosucced");
     maj.classList.add("succed");
-    score += 25;
+    score += 20;
   } else {
     maj.classList.remove("succed");
     maj.classList.add("nosucced");
   }
+  if (myinput.value.match(patternsymbole)) {
+    symb.classList.remove("nosucced");
+    symb.classList.add("succed");
+    score += 20;
+  } else {
+    symb.classList.remove("succed");
+    symb.classList.add("nosucced");
+  }
   if (myinput.value.match(patternmin)) {
     min.classList.remove("nosucced");
     min.classList.add("succed");
-    score += 25;
+    score += 20;
   } else {
     min.classList.remove("succed");
     min.classList.add("nosucced");
@@ -58,7 +71,7 @@ function verifMdp() {
   if (myinput.value.match(patternnbr)) {
     nbr.classList.remove("nosucced");
     nbr.classList.add("succed");
-    score += 25;
+    score += 20;
   } else {
     nbr.classList.remove("succed");
     nbr.classList.add("nosucced");
@@ -66,7 +79,7 @@ function verifMdp() {
   if (myinput.value.match(patternlongueur)) {
     longueur.classList.remove("nosucced");
     longueur.classList.add("succed");
-    score += 25;
+    score += 20;
   } else {
     longueur.classList.remove("succed");
     longueur.classList.add("nosucced");
@@ -91,7 +104,8 @@ function confirmMdp() {
 
 function activerBouton() {
   verifmail();
-  if (mdpFort && les2mdps && mailBon) {
+  verifusername();
+  if (mdpFort && les2mdps && mailBon && userBon) {
     bouton.removeAttribute("disabled");
   } else {
     bouton.setAttribute("disabled", "disabled");
@@ -123,14 +137,24 @@ function verifmail() {
   }
 }
 
+function verifusername() {
+  if (myinputUser.value.match(patternusername)) {
+    userBon = true;
+    myMessageerreur2.innerHTML = "";
+  } else {
+    userBon = false;
+    myMessageerreur2.innerHTML = "Username non valide (min 3 caractères)";
+  }
+}
+
 function miseAJourBarre(scoreCalculé) {
   barrePourcentage.style.width = scoreCalculé + "%";
   barrePourcentage.innerText = scoreCalculé + "%";
   barrePourcentage.className = "progress-bar progress-bar-striped";
 
-  if (scoreCalculé < 25) {
+  if (scoreCalculé < 40) {
     barrePourcentage.classList.add("bg-danger");
-  } else if (scoreCalculé < 50) {
+  } else if (scoreCalculé < 60) {
     barrePourcentage.classList.add("bg-warning");
   } else if (scoreCalculé < 100) {
     barrePourcentage.classList.add("bg-info");
