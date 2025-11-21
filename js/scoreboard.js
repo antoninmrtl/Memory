9f4c54279e3137ab8f5ad5c353e1076f28f4574c
antonin Martel <antonin.mrtl.pr@gmail.com>
2025-11-21 13:48:13 +0100

    Add bouton back function

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -52,27 +52,10 @@ myscoree2.forEach((partie) => {
   container.appendChild(stat);
 });
 
-// if (myscoree2.length > 0) {
-//   console.log(myscoree2[0].username);
-//   console.log(myscoree2[0].score);
-//   console.log(myscoree2[0].shots);
-//   console.log(myscoree2[0].time);
-//   console.log(myscoree2[0].size);
-//   console.log(myscoree2[0].date);
-// } else {
-//   console.log("Le tableau est vide ou la clé localStorage est incorrecte.");
-// }
+let mybouton = document.getElementById("bouton");
 
-// let myScore = document.getElementById("score");
-// let myUsername = document.getElementById("username");
-// let myShots = document.getElementById("shots");
-// let mySize = document.getElementById("size");
-// let myTime = document.getElementById("time");
-// let myDate = document.getElementById("date");
+mybouton.addEventListener("click", bouton);
 
-// myScore.innerText = myscoree2[myscoree2.length - 1].score;
-// myUsername.innerText = myscoree2[myscoree2.length - 1].username;
-// myShots.innerText = myscoree2[myscoree2.length - 1].shots;
-// mySize.innerText = myscoree2[myscoree2.length - 1].size;
-// myTime.innerText = myscoree2[myscoree2.length - 1].time;
-// myDate.innerText = myscoree2[myscoree2.length - 1].date;
+function bouton() {
+  document.location.href = "../pages/jouer.html";
+}
